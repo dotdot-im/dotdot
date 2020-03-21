d9fadf4bd54702d5485610bc61b192aa1270ff28
Alejandro U. Alvarez <aluralma@gmail.com>
2020-03-21 18:52:10 +0000

    Updates

---
+++ precache-manifest.a94cc677d91693e2990fd9e4cb00de51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969af0bc17647bc85c3f1ecccf1f14ba",
+    "url": "/hic-web/index.html"
+  },
+  {
+    "revision": "97d1dfb0ae94100a21dd",
+    "url": "/hic-web/static/css/main.e5bff07c.chunk.css"
+  },
+  {
+    "revision": "67ae6937c57dfac20d29",
+    "url": "/hic-web/static/js/2.9350d44d.chunk.js"
+  },
+  {
+    "revision": "76782b8eb0f434dc0abcc56e4ad414c6",
+    "url": "/hic-web/static/js/2.9350d44d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97d1dfb0ae94100a21dd",
+    "url": "/hic-web/static/js/main.ea98af26.chunk.js"
+  },
+  {
+    "revision": "ccd917ffb4f49d681fc4",
+    "url": "/hic-web/static/js/runtime-main.7099c503.js"
+  }
+]);

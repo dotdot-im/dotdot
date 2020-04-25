22b842448150db1e0907756af5113193bd1c6155
Jaime Caballero <me@jaicab.com>
2020-04-25 04:08:00 +0200

    Center loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -7,8 +7,15 @@ type Props = {
 
 export default (props: Props) => {
   return (
-    <>
+    <div
+      style={{
+        position: 'absolute',
+        top: '45%',
+        left: '50%',
+        transform: 'translateX(-50%) translateY(-50%)',
+      }}
+    >
       <Logo infinite />
-    </>
+    </div>
   )
 }

5c157f88d3433ad65b4713a943e8b5f624d7dd41
aurbano <aurbano@users.noreply.github.com>
2020-04-27 13:32:16 +0000

    deploy: eebaba40bb8b2b61662f51aa41edaa93ac930d48

---
+++ precache-manifest.0b50a730466bd09ffa4a3a02c9749b95.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfebab547313f42b5433fa4fe3a2bde4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d51de0a2abde422a40a6",
+    "url": "/static/css/main.e87c539e.chunk.css"
+  },
+  {
+    "revision": "379c1c7c845a66e04576",
+    "url": "/static/js/2.ce0a9bab.chunk.js"
+  },
+  {
+    "revision": "7639284f7257cddb798cf711b8ff06ad",
+    "url": "/static/js/2.ce0a9bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3bfd25745e7f4fdb69f",
+    "url": "/static/js/3.651223b1.chunk.js"
+  },
+  {
+    "revision": "11632a58e510552bf3ae8fe3d05874e6",
+    "url": "/static/js/3.651223b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51de0a2abde422a40a6",
+    "url": "/static/js/main.5b817f35.chunk.js"
+  },
+  {
+    "revision": "ed203f1bde97bb9d35a2",
+    "url": "/static/js/runtime-main.9780a9b4.js"
+  },
+  {
+    "revision": "482e2f99ca18e42ac8f8b60d1b7b76fe",
+    "url": "/static/media/newMessage.482e2f99.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eada19b102413e8944e9399a1a27bb9d.js"
+  "/precache-manifest.0b50a730466bd09ffa4a3a02c9749b95.js"
 );
 
 self.addEventListener('message', (event) => {

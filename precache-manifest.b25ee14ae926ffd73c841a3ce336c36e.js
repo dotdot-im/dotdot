5f5674fac2b2f52e62ed24ce9152ba71e7d39a20
jaicab <jaicab@users.noreply.github.com>
2020-04-28 11:13:12 +0000

    deploy: 2f2f5b2056ab88141cb76ddcb70da897215e8456

--- precache-manifest.b25ee14ae926ffd73c841a3ce336c36e.js
+++ precache-manifest.b25ee14ae926ffd73c841a3ce336c36e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ef3762a0bccc650c9ce2bab9bb95ce80",
+    "revision": "79f3113bbb5ea35efa11191a40f1480a",
     "url": "/index.html"
   },
   {
-    "revision": "35903157c524f132497c",
-    "url": "/static/css/main.e2f8e4a6.chunk.css"
+    "revision": "d229dd91412b72ff4a9a",
+    "url": "/static/css/main.9fa2c7de.chunk.css"
   },
   {
     "revision": "71e7e6e8ed80e8f34c54",
@@ -24,7 +24,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/3.829d8112.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "35903157c524f132497c",
+    "revision": "d229dd91412b72ff4a9a",
     "url": "/static/js/main.91fdc16d.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b54e2a8d5fe29a08eefd1c861a10665.js"
+  "/precache-manifest.b25ee14ae926ffd73c841a3ce336c36e.js"
 );
 
 self.addEventListener('message', (event) => {

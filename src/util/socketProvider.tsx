7024683ca6fbbc8d976201e70906beaee6f96cb8
Alejandro Alvarez <alejandro.alvarez@transferwise.com>
2021-02-06 20:41:24 +0000

    start migration to socketio 3

--- src/util/socketProvider.tsx
+++ src/util/socketProvider.tsx
@@ -1,33 +1,33 @@
-import React, { createContext, useEffect, useState } from 'react'
-import socketio from 'socket.io-client'
+import React, { createContext, useEffect, useState } from 'react';
+import socketio from 'socket.io-client';
 
-import { useGlobalState } from 'store/state'
-import { API_URL } from '../constants'
-import { Message, EVENTS } from 'store/types'
+import { useGlobalState } from 'store/state';
+import { API_URL } from '../constants';
+import { Message, EVENTS } from 'store/types';
 
-const USAGE_TIMER_INTERAL = 30 * 1000
+const USAGE_TIMER_INTERAL = 30 * 1000;
 
 type SocketContextType = {
-  socket: SocketIOClient.Socket | null
-}
+  socket: SocketIOClient.Socket | null;
+};
 
 export const SocketContext = createContext<SocketContextType>({
   socket: null,
-})
+});
 
 type Props = {
-  children: any
-}
+  children: any;
+};
 
 export default (props: Props) => {
-  const { state, dispatch } = useGlobalState()
-  const [socket, setSocket] = useState<SocketIOClient.Socket | null>(null)
+  const { state, dispatch } = useGlobalState();
+  const [socket, setSocket] = useState<SocketIOClient.Socket | null>(null);
 
   const hasPassword = state.auth.user?.hasPassword;
 
   useEffect(() => {
     if (!state.auth.loggedIn) {
-      return
+      return;
     }
 
     // socket already set up
@@ -39,92 +39,86 @@ export default (props: Props) => {
     dispatch({
       type: 'error',
       payload: null,
-    })
+    });
 
     // initialize the socket
     const newSocket = socketio(API_URL, {
       reconnection: true,
       timeout: 2000,
-    })
+    });
 
     setInterval(() => {
       if (newSocket.connected) {
-        newSocket.emit('timer')
+        newSocket.emit('timer');
       }
-    }, USAGE_TIMER_INTERAL)
+    }, USAGE_TIMER_INTERAL);
 
     // remind to set a password
     if (!hasPassword) {
       setTimeout(() => {
         dispatch({
           type: 'system_message',
-          payload: 'Remember: If you want to keep this username, you must set a password! You can set it by clicking on the lock icon on the top right of the screen.'
-        })
+          payload:
+            'Remember: If you want to keep this username, you must set a password! You can set it by clicking on the lock icon on the top right of the screen.',
+        });
       }, 60 * 1000);
     }
 
-    Object.values(EVENTS).forEach(event => {
+    Object.values(EVENTS).forEach((event) => {
       newSocket.on(event, (payload: any) => {
         dispatch({
           type: `socket_${event}`,
           payload: payload,
-        })
-      })
-    })
+        });
+      });
+    });
 
     newSocket.on('stats', (payload: Message[]) => {
       dispatch({
         type: 'stats',
         payload,
-      })
-    })
-
-    newSocket.on('connect_error', () => {
-      dispatch({
-        type: 'offline',
-        payload: null,
-      })
-    })
+      });
+    });
 
     newSocket.on('connect_timeout', () => {
       dispatch({
         type: 'offline',
         payload: null,
-      })
-    })
+      });
+    });
 
-    newSocket.on('error', (error: string) => {
+    newSocket.on('connect_error', (error: string) => {
       dispatch({
         type: `socket_${EVENTS.DISCONNECT}`,
-      })
+      });
 
       if (error) {
         dispatch({
           type: 'error',
           payload: error,
-        })
+        });
         dispatch({
           type: 'login',
           payload: null,
-        })
+        });
       } else {
         dispatch({
           type: 'offline',
           payload: null,
-        })
+        });
       }
-    })
+    });
 
-    setSocket(newSocket)
-  }, [dispatch, socket, state.auth.loggedIn, hasPassword])
+    setSocket(newSocket);
+  }, [dispatch, socket, state.auth.loggedIn, hasPassword]);
 
   let provider = {
     socket,
-  }
+  };
 
   return (
     <SocketContext.Provider value={provider}>
       {props.children}
     </SocketContext.Provider>
-  )
-}
+  );
+};
